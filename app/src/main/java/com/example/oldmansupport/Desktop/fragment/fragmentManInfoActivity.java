bd0a5347c347b91e69f9e11de8e90f5c3c469f32
li18051719 <2891173256@qq.com>
2020-05-22 14:46:11 +0800

    +紧急联系时发送位置给紧急联系人

--- app/src/main/java/com/example/oldmansupport/Desktop/fragment/fragmentManInfoActivity.java
+++ app/src/main/java/com/example/oldmansupport/Desktop/fragment/fragmentManInfoActivity.java
@@ -1,15 +1,22 @@
 package com.example.oldmansupport.Desktop.fragment;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,6 +24,8 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.oldmansupport.R;
 import com.example.oldmansupport.li.Map_getPosition;
+
+import java.util.List;
 
 import static com.example.oldmansupport.MainActivity.location;
 
@@ -83,8 +92,9 @@ public class fragmentManInfoActivity extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:"+110));
+                intent.setData(Uri.parse("tel:"+"18166139071"));
                 startActivity(intent);
+                sendSMS("18166139071",location,getActivity());
             }
         });
 
@@ -110,4 +120,54 @@ public class fragmentManInfoActivity extends Fragment {
 //
 //    }
 
+    //直接调用短信接口发短信
+    public void sendSMS(String phoneNumber, String message, final Context context){
+        //处理返回的发送状态
+        String SENT_SMS_ACTION = "SENT_SMS_ACTION";
+        Intent sentIntent = new Intent(SENT_SMS_ACTION);
+        PendingIntent sentPI = PendingIntent.getBroadcast(context, 0, sentIntent,
+                0);
+        // register the Broadcast Receivers
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context _context, Intent _intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(context,
+                                "短信发送成功", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT_SMS_ACTION));
+
+        //处理返回的接收状态
+        String DELIVERED_SMS_ACTION = "DELIVERED_SMS_ACTION";
+        // create the deilverIntent parameter
+        Intent deliverIntent = new Intent(DELIVERED_SMS_ACTION);
+        PendingIntent deliverPI = PendingIntent.getBroadcast(context, 0,
+                deliverIntent, 0);
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context _context, Intent _intent) {
+                Toast.makeText(context,
+                        "收信人已经成功接收", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }, new IntentFilter(DELIVERED_SMS_ACTION));
+
+        //获取短信管理器
+        android.telephony.SmsManager smsManager = android.telephony.SmsManager.getDefault();
+        //拆分短信内容（手机短信长度限制）
+        List<String> divideContents = smsManager.divideMessage(message);
+        for (String text : divideContents) {
+            smsManager.sendTextMessage(phoneNumber, null, text, sentPI, deliverPI);
+        }
+    }
 }
